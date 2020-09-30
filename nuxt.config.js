f64382c2ee02997c23761bd12165f3123f6da7a6
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-10-01 01:09:02 +0900

    add:vue-codemirrorを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,15 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['normalize.css','@/config/common.css'
+  css: [
+    'normalize.css',
+    '@/config/common.css',
+    'codemirror/lib/codemirror.css',
+    'codemirror/theme/base16-dark.css',
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
+plugins: [ {src: '@/plugins/nuxt-codemirror-plugin', ssr: false }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,53 @@
 <template>
   <div class="container">
       <div class="markdown-edit">
-        <h2>edit</h2>
+        <h2 class="notice">edit</h2>
+        <codemirror />
       </div>
       <div class="markdown-preview">
-        <h2>preview</h2>
+        <h2 class="notice">preview</h2>
+        <div class="markdown-output"></div>
       </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+}
 </script>
 
 <style>
+/*
 .container {
-  margin: 0 auto;
+  margin: 3rem auto;
   display: flex;
   justify-content: space-between;
-  text-align: center;
+  max-width: 1100px;
 }
 .markdown-edit {
-  width: 50%;
+  width: 48%;
 }
 .markdown-preview {
-  width: 50%;
+  width: 48%;
 }
-
+.notice {
+  background: #323232;
+  color: #fff;
+  padding: 0.6rem 1.2rem;
+  border-radius: 8px 8px 0 0 ;
+}
+.markdown-input {
+  width: 100%;
+  resize: none;
+  border: none;
+  padding: 1.2rem;
+  min-height: 10rem;
+}
+.markdown-output {
+  width: 100;
+  background: #fff;
+  min-height: 10rem;
+}
+*/
 </style>
